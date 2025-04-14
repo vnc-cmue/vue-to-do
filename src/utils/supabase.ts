0ef60ec105bcabf0819aeff02165b7c30f773a0a
vnc-cmue <christian.mueller@vancado.de>
2025-04-14 15:36:55 +0200

    [FEATURE] implemented supabase database and and func in store todo

---
+++ src/utils/supabase.ts
@@ -0,0 +1,7 @@
+// utils/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
