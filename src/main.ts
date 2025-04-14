c8884e0049f700e33c90c8ef96b34d7f9ccd943e
vnc-cmue <christian.mueller@vancado.de>
2025-04-14 16:44:38 +0200

    Updates

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-
-createApp(App).use(createPinia()).mount('#app')
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// tailwind.config.js
-export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')], 
-}
