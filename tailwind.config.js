4e72318ab36a5a8cdf9ffa33353008e7df645274
vnc-cmue <christian.mueller@vancado.de>
2025-04-19 11:47:09 +0200

    [TASK] add env(safe-area-inset-bottom)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
